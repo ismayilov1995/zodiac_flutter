b7af4f0b89622cf860e01f5ca648db41ac4901d5
ismayilov1995 <ismayilov1995@gmail.com>
2020-08-16 00:29:04 +0400

    Add utils and images

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,3 @@ import 'package:zodiac_app/navigation.dart';
 void main() {
   runApp(AppNavigation());
 }
-
-//class MyApp extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return AppNavigation();
-//  }
-//}
